20801dadb236e4f13aec9f063bb4babd65640d70
Anuka Hettiarachchi <anuka3382@gmail.com>
2026-04-28 00:49:34 +0530

    Add NR-to-NR distance and RSRP handover example scenarios

---
+++ examples/oran-nr-2-nr-rsrp-handover-lm-example.cc
@@ -0,0 +1,342 @@
+/**
+ * NIST-developed software is provided by NIST as a public service. You may
+ * use, copy and distribute copies of the software in any medium, provided that
+ * you keep intact this entire notice. You may improve, modify and create
+ * derivative works of the software or any portion of the software, and you may
+ * copy and distribute such modifications or works. Modified works should carry
+ * a notice stating that you changed the software and should note the date and
+ * nature of any such change. Please explicitly acknowledge the National
+ * Institute of Standards and Technology as the source of the software.
+ *
+ * NIST-developed software is expressly provided "AS IS." NIST MAKES NO
+ * WARRANTY OF ANY KIND, EXPRESS, IMPLIED, IN FACT OR ARISING BY OPERATION OF
+ * LAW, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTY OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT AND DATA ACCURACY. NIST
+ * NEITHER REPRESENTS NOR WARRANTS THAT THE OPERATION OF THE SOFTWARE WILL BE
+ * UNINTERRUPTED OR ERROR-FREE, OR THAT ANY DEFECTS WILL BE CORRECTED. NIST
+ * DOES NOT WARRANT OR MAKE ANY REPRESENTATIONS REGARDING THE USE OF THE
+ * SOFTWARE OR THE RESULTS THEREOF, INCLUDING BUT NOT LIMITED TO THE
+ * CORRECTNESS, ACCURACY, RELIABILITY, OR USEFULNESS OF THE SOFTWARE.
+ *
+ * You are solely responsible for determining the appropriateness of using and
+ * distributing the software and you assume all risks associated with its use,
+ * including but not limited to the risks and costs of program errors,
+ * compliance with applicable laws, damage to or loss of data, programs or
+ * equipment, and the unavailability or interruption of operation. This
+ * software is not intended to be used in any situation where a failure could
+ * cause risk of injury or damage to property. The software developed by NIST
+ * employees is not subject to copyright protection within the United States.
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/isotropic-antenna-model.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/nr-channel-helper.h"
+#include "ns3/nr-helper.h"
+#include "ns3/nr-point-to-point-epc-helper.h"
+#include "ns3/nr-ue-net-device.h"
+#include "ns3/nr-ue-phy.h"
+#include "ns3/oran-module.h"
+#include "ns3/point-to-point-module.h"
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("OranNr2NrRsrpHandoverExample");
+
+/**
+ * Example of the O-RAN NR bridge using RSRP-based handover.
+ *
+ * The scenario: 1 NR UE moves back and forth between 2 NR gNBs.
+ * The UE reports location, cell ID, and RSRP/RSRQ measurements to the RIC.
+ * The OranLmNr2NrRsrpHandover Logic Module periodically inspects the latest
+ * RSRP per neighbour cell and issues an OranCommandNr2NrHandover whenever a
+ * neighbour's RSRP exceeds the serving cell's. The gNB-side automatic handover
+ * algorithm is disabled (NrNoOpHandoverAlgorithm) so all handovers come from
+ * the RIC.
+ */
+
+void
+NotifyHandoverEndOkUe(std::string context, uint64_t imsi, uint16_t cellid, uint16_t rnti)
+{
+    std::cout << Simulator::Now().GetSeconds() << " " << context << " UE IMSI " << imsi
+              << ": completed handover to CellId " << cellid << " RNTI " << rnti << std::endl;
+}
+
+void
+ReverseVelocity(NodeContainer nodes, Time interval)
+{
+    for (uint32_t idx = 0; idx < nodes.GetN(); idx++)
+    {
+        Ptr<ConstantVelocityMobilityModel> mobility =
+            nodes.Get(idx)->GetObject<ConstantVelocityMobilityModel>();
+        mobility->SetVelocity(Vector(mobility->GetVelocity().x * -1, 0, 0));
+    }
+
+    Simulator::Schedule(interval, &ReverseVelocity, nodes, interval);
+}
+
+void
+QueryRcSink(std::string query, std::string args, int rc)
+{
+    std::cout << Simulator::Now().GetSeconds() << " Query "
+              << ((rc == SQLITE_OK || rc == SQLITE_DONE) ? "OK" : "ERROR") << "(" << rc << "): \""
+              << query << "\"";
+    if (!args.empty())
+    {
+        std::cout << " (" << args << ")";
+    }
+    std::cout << std::endl;
+}
+
+int
+main(int argc, char* argv[])
+{
+    uint16_t numberOfUes = 1;
+    uint16_t numberOfGnbs = 2;
+    Time simTime = Seconds(50);
+    double distance = 200;
+    Time interval = Seconds(15);
+    double speed = 15;
+    bool verbose = false;
+    Time lmQueryInterval = Seconds(5);
+    std::string dbFileName = "oran-repository.db";
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("verbose", "Enable printing SQL queries results", verbose);
+    cmd.AddValue("sim-time", "Simulation duration", simTime);
+    cmd.AddValue("lm-query-interval", "LM query interval", lmQueryInterval);
+    cmd.Parse(argc, argv);
+
+    Config::SetDefault("ns3::NrGnbPhy::TxPower", DoubleValue(30));
+    Config::SetDefault("ns3::NrUePhy::TxPower", DoubleValue(23));
+    Config::SetDefault("ns3::NrUePhy::EnableUplinkPowerControl", BooleanValue(false));
+
+    Ptr<NrHelper> nrHelper = CreateObject<NrHelper>();
+    Ptr<NrPointToPointEpcHelper> epcHelper = CreateObject<NrPointToPointEpcHelper>();
+    nrHelper->SetEpcHelper(epcHelper);
+    nrHelper->SetAttribute("UseIdealRrc", BooleanValue(true));
+
+    nrHelper->SetHandoverAlgorithmType("ns3::NrNoOpHandoverAlgorithm");
+
+    NodeContainer gnbNodes;
+    NodeContainer ueNodes;
+    gnbNodes.Create(numberOfGnbs);
+    ueNodes.Create(numberOfUes);
+
+    Ptr<ListPositionAllocator> gnbPosAlloc = CreateObject<ListPositionAllocator>();
+    for (uint16_t i = 0; i < numberOfGnbs; i++)
+    {
+        gnbPosAlloc->Add(Vector(distance * i, 0, 25));
+    }
+
+    MobilityHelper gnbMobility;
+    gnbMobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    gnbMobility.SetPositionAllocator(gnbPosAlloc);
+    gnbMobility.Install(gnbNodes);
+
+    MobilityHelper ueMobility;
+    ueMobility.SetMobilityModel("ns3::ConstantVelocityMobilityModel");
+    ueMobility.Install(ueNodes);
+
+    for (uint16_t i = 0; i < numberOfUes; i++)
+    {
+        Ptr<ConstantVelocityMobilityModel> mob =
+            ueNodes.Get(i)->GetObject<ConstantVelocityMobilityModel>();
+        mob->SetPosition(
+            Vector((distance / 2) - (speed * (interval.GetSeconds() / 2)), 0, 1.5));
+        mob->SetVelocity(Vector(speed, 0, 0));
+    }
+
+    Simulator::Schedule(interval, &ReverseVelocity, ueNodes, interval);
+
+    nrHelper->SetUeAntennaTypeId(IsotropicAntennaModel::GetTypeId().GetName());
+    nrHelper->SetGnbAntennaTypeId(IsotropicAntennaModel::GetTypeId().GetName());
+
+    Ptr<NrChannelHelper> channelHelper = CreateObject<NrChannelHelper>();
+    channelHelper->ConfigurePropagationFactory(FriisPropagationLossModel::GetTypeId());
+
+    CcBwpCreator ccBwpCreator;
+    CcBwpCreator::SimpleOperationBandConf bandConf(2.8e9, 10e6, static_cast<uint8_t>(1));
+    OperationBandInfo band = ccBwpCreator.CreateOperationBandContiguousCc(bandConf);
+    channelHelper->AssignChannelsToBands({band});
+
+    BandwidthPartInfoPtrVector allBwps = CcBwpCreator::GetAllBwps({band});
+
+    NetDeviceContainer gnbDevs = nrHelper->InstallGnbDevice(gnbNodes, allBwps);
+    NetDeviceContainer ueDevs = nrHelper->InstallUeDevice(ueNodes, allBwps);
+
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create(1);
+    Ptr<Node> remoteHost = remoteHostContainer.Get(0);
+    InternetStackHelper internet;
+    internet.Install(remoteHostContainer);
+
+    PointToPointHelper p2ph;
+    p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
+    p2ph.SetDeviceAttribute("Mtu", UintegerValue(1500));
+    p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+    Ptr<Node> pgw = epcHelper->GetPgwNode();
+    NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase("1.0.0.0", "255.0.0.0");
+    ipv4h.Assign(internetDevices);
+
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting =
+        ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
+    remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
+
+    internet.Install(ueNodes);
+    Ipv4InterfaceContainer ueIpIfaces =
+        epcHelper->AssignUeIpv4Address(NetDeviceContainer(ueDevs));
+
+    nrHelper->AddX2Interface(gnbNodes);
+    nrHelper->AttachToGnb(ueDevs.Get(0), gnbDevs.Get(0));
+
+    uint16_t dlPort = 10000;
+    UdpClientHelper dlClientHelper(ueIpIfaces.GetAddress(0), dlPort);
+    dlClientHelper.SetAttribute("Interval", TimeValue(MilliSeconds(10)));
+    dlClientHelper.SetAttribute("MaxPackets", UintegerValue(100000));
+    dlClientHelper.SetAttribute("PacketSize", UintegerValue(512));
+    ApplicationContainer clientApps = dlClientHelper.Install(remoteHost);
+    clientApps.Start(Seconds(1.0));
+
+    PacketSinkHelper dlPacketSinkHelper("ns3::UdpSocketFactory",
+                                        InetSocketAddress(Ipv4Address::GetAny(), dlPort));
+    ApplicationContainer serverApps = dlPacketSinkHelper.Install(ueNodes.Get(0));
+    serverApps.Start(Seconds(0.5));
+
+    // O-RAN setup
+    if (!dbFileName.empty())
+    {
+        std::remove(dbFileName.c_str());
+    }
+
+    Ptr<OranNearRtRic> nearRtRic = nullptr;
+    OranE2NodeTerminatorContainer e2NodeTerminatorsGnbs;
+    OranE2NodeTerminatorContainer e2NodeTerminatorsUes;
+    Ptr<OranHelper> oranHelper = CreateObject<OranHelper>();
+
+    oranHelper->SetAttribute("Verbose", BooleanValue(true));
+    oranHelper->SetAttribute("LmQueryInterval", TimeValue(lmQueryInterval));
+    oranHelper->SetAttribute("E2NodeInactivityThreshold", TimeValue(Seconds(2)));
+    oranHelper->SetAttribute("E2NodeInactivityIntervalRv",
+                             StringValue("ns3::ConstantRandomVariable[Constant=2]"));
+    oranHelper->SetAttribute("LmQueryMaxWaitTime", TimeValue(Seconds(0)));
+    oranHelper->SetAttribute("LmQueryLateCommandPolicy", EnumValue(OranNearRtRic::DROP));
+    oranHelper->SetAttribute("RicTransmissionDelayRv",
+                             StringValue("ns3::ConstantRandomVariable[Constant=0.001]"));
+
+    oranHelper->SetDataRepository("ns3::OranDataRepositorySqlite",
+                                  "DatabaseFile",
+                                  StringValue(dbFileName));
+
+    oranHelper->SetDefaultLogicModule("ns3::OranLmNr2NrRsrpHandover",
+                                      "ProcessingDelayRv",
+                                      StringValue("ns3::ConstantRandomVariable[Constant=0]"));
+
+    oranHelper->SetConflictMitigationModule("ns3::OranCmmNoop");
+
+    nearRtRic = oranHelper->CreateNearRtRic();
+
+    // NR UE terminators — built manually so we can connect the NrUePhy
+    // ReportUeMeasurements trace into the RSRP/RSRQ reporter.
+    for (uint32_t idx = 0; idx < ueNodes.GetN(); idx++)
+    {
+        Ptr<OranReporterLocation> locationReporter = CreateObject<OranReporterLocation>();
+        Ptr<OranReporterNrUeCellInfo> cellInfoReporter =
+            CreateObject<OranReporterNrUeCellInfo>();
+        Ptr<OranReporterNrUeRsrpRsrq> rsrpRsrqReporter =
+            CreateObject<OranReporterNrUeRsrpRsrq>();
+        Ptr<OranE2NodeTerminatorNrUe> nrUeTerminator =
+            CreateObject<OranE2NodeTerminatorNrUe>();
+
+        locationReporter->SetAttribute("Terminator", PointerValue(nrUeTerminator));
+        locationReporter->SetAttribute("Trigger",
+                                       StringValue("ns3::OranReportTriggerPeriodic"));
+
+        cellInfoReporter->SetAttribute("Terminator", PointerValue(nrUeTerminator));
+        cellInfoReporter->SetAttribute(
+            "Trigger",
+            StringValue("ns3::OranReportTriggerNrUeHandover[InitialReport=true]"));
+
+        rsrpRsrqReporter->SetAttribute("Terminator", PointerValue(nrUeTerminator));
+        rsrpRsrqReporter->SetAttribute("Trigger",
+                                       StringValue("ns3::OranReportTriggerPeriodic"));
+
+        // Connect NrUePhy::ReportUeMeasurements → reporter
+        for (uint32_t d = 0; d < ueNodes.Get(idx)->GetNDevices(); d++)
+        {
+            Ptr<NrUeNetDevice> nrUeDev =
+                ueNodes.Get(idx)->GetDevice(d)->GetObject<NrUeNetDevice>();
+            if (nrUeDev)
+            {
+                Ptr<NrUePhy> uePhy = nrUeDev->GetPhy(0);
+                uePhy->TraceConnectWithoutContext(
+                    "ReportUeMeasurements",
+                    MakeCallback(&OranReporterNrUeRsrpRsrq::ReportRsrpRsrq, rsrpRsrqReporter));
+                break;
+            }
+        }
+
+        nrUeTerminator->SetAttribute("NearRtRic", PointerValue(nearRtRic));
+        nrUeTerminator->SetAttribute("RegistrationIntervalRv",
+                                     StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+        nrUeTerminator->SetAttribute("SendIntervalRv",
+                                     StringValue("ns3::ConstantRandomVariable[Constant=1]"));
+        nrUeTerminator->SetAttribute("TransmissionDelayRv",
+                                     StringValue("ns3::ConstantRandomVariable[Constant=0.001]"));
+
+        nrUeTerminator->AddReporter(locationReporter);
+        nrUeTerminator->AddReporter(cellInfoReporter);
+        nrUeTerminator->AddReporter(rsrpRsrqReporter);
+
+        nrUeTerminator->Attach(ueNodes.Get(idx));
+        e2NodeTerminatorsUes.Add(nrUeTerminator);
+    }
+
+    // NR gNB terminators — straightforward via OranHelper
+    oranHelper->SetE2NodeTerminator("ns3::OranE2NodeTerminatorNrGnb",
+                                    "RegistrationIntervalRv",
+                                    StringValue("ns3::ConstantRandomVariable[Constant=1]"),
+                                    "SendIntervalRv",
+                                    StringValue("ns3::ConstantRandomVariable[Constant=1]"),
+                                    "TransmissionDelayRv",
+                                    StringValue("ns3::ConstantRandomVariable[Constant=0.001]"));
+
+    oranHelper->AddReporter("ns3::OranReporterLocation",
+                            "Trigger",
+                            StringValue("ns3::OranReportTriggerPeriodic"));
+
+    e2NodeTerminatorsGnbs.Add(oranHelper->DeployTerminators(nearRtRic, gnbNodes));
+
+    if (verbose)
+    {
+        nearRtRic->Data()->TraceConnectWithoutContext("QueryRc", MakeCallback(&QueryRcSink));
+    }
+
+    Simulator::Schedule(Seconds(1),
+                        &OranHelper::ActivateAndStartNearRtRic,
+                        oranHelper,
+                        nearRtRic);
+    Simulator::Schedule(Seconds(1.5),
+                        &OranHelper::ActivateE2NodeTerminators,
+                        oranHelper,
+                        e2NodeTerminatorsGnbs);
+    Simulator::Schedule(Seconds(2),
+                        &OranHelper::ActivateE2NodeTerminators,
+                        oranHelper,
+                        e2NodeTerminatorsUes);
+
+    Config::Connect("/NodeList/*/DeviceList/*/NrUeRrc/HandoverEndOk",
+                    MakeCallback(&NotifyHandoverEndOkUe));
+
+    Simulator::Stop(simTime);
+    Simulator::Run();
+    Simulator::Destroy();
+
+    return 0;
+}
